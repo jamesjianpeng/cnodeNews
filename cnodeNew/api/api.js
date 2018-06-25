5e5b246e9ce906d3330cee6de1e28eaee639f1cd
彭涧 <jamesjianpeng@gmail.com>
2018-06-25 10:56:59 +0800

    perf(api.js): Add 4 apis
    1. Message interface 2. Message count interface 3. Like interface 4. Cancel Like interface

--- cnodeNew/api/api.js
+++ cnodeNew/api/api.js
@@ -1,10 +1,14 @@
 const baseURL = 'https://cnodenewapi.pengjiandry.com';
 
 const api = {
-  topics: '/topics',
-  topicDetail: '/topicDetail/',
-  login: '/login',
-  user: '/user/'
+  topics: '/topics', // 主题列表
+  topicDetail: '/topicDetail/', // 主题详情
+  login: '/login', // 登录
+  user: '/user/', // 用户信息
+  messages: '/messages', // 信息列表
+  messageCount: '/messageCount', // 信息数量
+  collect: '/collect', // 收藏
+  delCollect: '/delCollect' // 取消收藏
 }
 
 for (let key in api) {
