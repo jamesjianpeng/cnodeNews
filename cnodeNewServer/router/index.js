dfd70eda3e06a559b002fa00aa9fdc49139d2c5e
彭涧 <jamesjianpeng@gmail.com>
2018-06-25 10:48:15 +0800

    perf(Router): Add 3 interfaces
    1. Message interfaces 2. Like interfaces 3. Cancel like interfaces

--- cnodeNewServer/router/index.js
+++ cnodeNewServer/router/index.js
@@ -15,7 +15,10 @@ router.get('/', function(req, res) {
 
 // 主题列表
 router.get('/topics', function(req, res) {
-  axios.get(`${baseURL}/topics`)
+  const params = req.query
+  axios.get(`${baseURL}/topics`, {
+    params
+  })
   .then((response) => {
     if (response.status === 200) {
       res.json(response.data);
@@ -29,7 +32,12 @@ router.get('/topics', function(req, res) {
 
 // 主题详情
 router.get('/topicDetail/:id', function(req, res) {
-  axios.get(`${baseURL}/topic/${req.params.id}`)
+  const { accessToken: accesstoken } = req.query
+  axios.get(`${baseURL}/topic/${req.params.id}`, {
+    params: {
+      accesstoken
+    }
+  })
   .then((response) => {
     if (response.status === 200) {
       res.json(response.data);
@@ -44,7 +52,6 @@ router.get('/topicDetail/:id', function(req, res) {
 // 登录
 router.post('/login', function(req, res) {
   const { token: accesstoken } = req.body;
-  console.log(accesstoken);
   axios.post(`${baseURL}/accesstoken`, {
     accesstoken
   })
@@ -63,6 +70,67 @@ router.post('/login', function(req, res) {
 router.get('/user/:name', function(req, res) {
   const { name } = req.params
   axios.get(`${baseURL}/user/${name}`)
+  .then((response) => {
+    if (response.status === 200) {
+      res.json(response.data);
+    }
+  })
+  .catch(err => {
+    // console.log(err);
+    res.send('error');
+  })
+});
+
+// 获取评论的消息列表
+router.get('/messages', function(req, res) {
+  const { accessToken: accesstoken } = req.query
+  axios.get(`${baseURL}/messages`, {
+    params: {
+      accesstoken
+    }
+  })
+  .then((response) => {
+    if (response.status === 200) {
+      res.json(response.data);
+    }
+  })
+  .catch(err => {
+    // console.log(err);
+    res.send('error');
+  })
+});
+
+// 主题的取消点赞
+router.post('/delCollect', function(req, res) {
+  const { 
+    accessToken: accesstoken,
+    topicId: topic_id
+  } = req.body
+  axios.post(`${baseURL}/topic_collect/de_collect`, {
+    accesstoken,
+    topic_id
+  })
+  .then((response) => {
+    if (response.status === 200) {
+      res.json(response.data);
+    }
+  })
+  .catch(err => {
+    // console.log(err);
+    res.send('error');
+  })
+});
+
+// 主题的点赞
+router.post('/collect', function(req, res) {
+  const { 
+    accessToken: accesstoken,
+    topicId: topic_id
+  } = req.body
+  axios.post(`${baseURL}/topic_collect/collect`, {
+    accesstoken,
+    topic_id
+  })
   .then((response) => {
     if (response.status === 200) {
       res.json(response.data);
